8ce6035d8419db50e08bbf316ae25473777f5593
Chris Davenport <chris@christopherdavenport.tech>
2019-09-04 12:30:22 -0700

    Compile Tests

--- core/src/test/scala/io/chrisdavenport/keysemaphore/SimpleKeySemaphoreSpec.scala
+++ core/src/test/scala/io/chrisdavenport/keysemaphore/SimpleKeySemaphoreSpec.scala
@@ -4,12 +4,12 @@ import org.specs2.mutable.Specification
 import cats.effect._
 import scala.concurrent.ExecutionContext.global
 
-class KeySemaphoreSpec extends Specification {
+class SimpleKeySemaphoreSpec extends Specification {
   "KeySemaphore" should {
     "only take the maximum values per key" in {
       implicit val CS = IO.contextShift(global)
       val test = for {
-        sem <- KeySemaphore.of[IO, Unit]{_: Unit => 1L}
+        sem <- SimpleKeySemaphore.of[IO, Unit]{_: Unit => 1L}
         first <- sem.tryAcquire.run(())
         second <- sem.tryAcquire.run(())
       } yield (first, second)
@@ -19,7 +19,7 @@ class KeySemaphoreSpec extends Specification {
     "not be affected by other keys" in {
       implicit val CS = IO.contextShift(global)
       val test = for {
-        sem <- KeySemaphore.of[IO, Int]{_: Int => 1L}
+        sem <- SimpleKeySemaphore.of[IO, Int]{_: Int => 1L}
         first <- sem.tryAcquire.run(1)
         second <- sem.tryAcquire.run(2)
         third <- sem.tryAcquire.run(1)
